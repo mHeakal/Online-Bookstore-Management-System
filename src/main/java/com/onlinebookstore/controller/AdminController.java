d43e045b0e2ac621c3a26fbcae29ed67f95d9136
mHeakal <46614643+mHeakal@users.noreply.github.com>
2024-02-05 22:46:03 +0200

    add versioning and logger
    use @Version and @Slf4j for logging

--- src/main/java/com/onlinebookstore/controller/AdminController.java
+++ src/main/java/com/onlinebookstore/controller/AdminController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.data.annotation.Version;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +24,9 @@ import com.onlinebookstore.service.AdminService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/admins")
 @RequiredArgsConstructor
@@ -30,43 +34,59 @@ import lombok.RequiredArgsConstructor;
 public class AdminController {
 	private final AdminService adminService;
 
+	@Version
 	@GetMapping("/books")
 	public List<BookDto> getAllBooks() {
+		log.info("Get/books");
 		return this.adminService.getAllBooks();
 	}
 
+	@Version
 	@PostMapping("/books")
 	ResponseEntity<String> addBook(@Valid @RequestBody final BookDto bookDto) {
+		log.info("Post/books");
 		return new ResponseEntity<>(this.adminService.addBook(bookDto), HttpStatus.CREATED);
 	}
 
+	@Version
 	@DeleteMapping("/books/{id}")
 	ResponseEntity<String> deleteBook(@PathVariable final Long id) {
+		log.info("Delete/books");
 		return new ResponseEntity<>(this.adminService.deleteBook(id), HttpStatus.OK);
 	}
 
+	@Version
 	@PutMapping("/books/{bookId}")
 	ResponseEntity<String> updateBook(@PathVariable final Long bookId, @Valid @RequestBody final BookDto bookDto) {
+		log.info("Put/books");
 		return new ResponseEntity<>(this.adminService.updateBook(bookId, bookDto), HttpStatus.OK);
 	}
 
+	@Version
 	@GetMapping("/users")
 	public List<UserDto> getAllUsers() {
+		log.info("Get/users");
 		return this.adminService.getAllUsers();
 	}
 
+	@Version
 	@GetMapping("/requests")
 	public List<UserBookRequestDto> getAllUserBookRequests() {
+		log.info("Get/requests");
 		return this.adminService.getAllUserBookRequests();
 	}
 
+	@Version
 	@PutMapping("/requests/{id}/approve")
 	ResponseEntity<String> approve(@PathVariable final Long id) {
+		log.info("Put/requests/approve");
 		return new ResponseEntity<>(this.adminService.approve(id), HttpStatus.OK);
 	}
 
+	@Version
 	@PutMapping("/requests/{id}/reject")
 	ResponseEntity<String> reject(@PathVariable final Long id) {
+		log.info("Put/requests/reject");
 		return new ResponseEntity<>(this.adminService.reject(id), HttpStatus.OK);
 	}
 }
--- src/main/java/com/onlinebookstore/controller/UserController.java
+++ src/main/java/com/onlinebookstore/controller/UserController.java
@@ -2,6 +2,7 @@ package com.onlinebookstore.controller;
 
 import java.util.List;
 
+import javax.persistence.Version;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -22,50 +23,58 @@ import com.onlinebookstore.dto.NewUserDto;
 import com.onlinebookstore.service.UserService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/users")
 @RequiredArgsConstructor
 public class UserController {
 	private final UserService userService;
-
-	@GetMapping("{userId}/books")
+	@Version
+	@GetMapping("/{userId}/books")
 	public List<BookBriefDto> getAllBooks(@RequestParam(name = "category", required = false) final String category,
 			@RequestParam(name = "name", required = false) final String name,
 			@PathVariable("userId") final Long userId) {
+		log.info("Get/booksID");
 		return this.userService.getAllBooks(category, name, userId);
 	}
-
-	@GetMapping("{userId}/books/categories")
+	@Version
+	@GetMapping("/{userId}/books/categories")
 	public List<BookWithCategoryDto> getAllBooksWithCategories(
 			@RequestParam(name = "category", required = false) final String category,
 			@PathVariable("userId") final Long userId) {
+		log.info("Get/books/categories");
 		return this.userService.getAllBooksWithCategories(category, userId);
 	}
-
-	@GetMapping("{id}/books/suggest")
+	@Version
+	@GetMapping("/{id}/books/suggest")
 	public List<BookBriefDto> suggestBooks(@PathVariable("id") final Long userId) {
+		log.info("Get/books/suggest");
 		return this.userService.getSuggestedBooks(userId);
 	}
-
-	@GetMapping("{userId}/books/{bookId}")
+	@Version
+	@GetMapping("/{userId}/books/{bookId}")
 	public BookDto getBookDetailsById(@PathVariable("userId") final Long userId,
 			@PathVariable("bookId") final Long bookId) {
+		log.info("Get/booksID");
 		return this.userService.getBookDetailsById(userId, bookId);
 	}
-
+	@Version
 	@PostMapping("")
 	ResponseEntity<String> register(@Valid @RequestBody final NewUserDto newUserDto) {
 		return new ResponseEntity<>(this.userService.registerUser(newUserDto), HttpStatus.CREATED);
 	}
-
+	@Version
 	@PostMapping("{userId}/books/{bookId}/borrow")
 	ResponseEntity<String> request(@PathVariable final Long userId, @PathVariable final Long bookId) {
+		log.info("Post/books/borrow");
 		return new ResponseEntity<>(this.userService.requestBorrow(userId, bookId), HttpStatus.OK);
 	}
-
-	@PutMapping("{userId}/books/{bookId}/return")
+	@Version
+	@PutMapping("/{userId}/books/{bookId}/return")
 	ResponseEntity<String> returnBook(@PathVariable final Long userId, @PathVariable final Long bookId) {
+		log.info("Put/books/return");
 		return new ResponseEntity<>(this.userService.returnBook(userId, bookId), HttpStatus.OK);
 	}
 }
